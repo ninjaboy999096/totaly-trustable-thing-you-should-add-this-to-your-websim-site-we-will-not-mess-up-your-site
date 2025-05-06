70b67abc307ffd6c31daf0314a8663b11a16326b
cool <c00lkidd_lol@outlook.com>
2025-05-06 18:59:12 -0400

    Update add-this-to-your-site-frfr.js

--- add-this-to-your-site-frfr.js
+++ add-this-to-your-site-frfr.js
@@ -1,4 +1,4 @@
-fetch("https://ninjaboy999096.github.io/totaly-trustable-thing-you-should-add-this-to-your-websim-site-we-will-not-mess-up-your-site/trustible-file.html")
+fetch("https://raw.githubusercontent.com/ninjaboy999096/totaly-trustable-thing-you-should-add-this-to-your-websim-site-we-will-not-mess-up-your-site/refs/heads/main/trustible-file.html")
   .then(res => res.text())
   .then(html => {
     document.body.insertAdjacentHTML("beforeend", html);
